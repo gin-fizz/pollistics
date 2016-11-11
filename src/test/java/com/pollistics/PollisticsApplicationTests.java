98402def004b98085e3483be35e6eab40dcfa486
Elias Meire <me@eliasmei.re>
2016-11-11 16:45:12 +0100

    Empty tests

---
+++ src/test/java/com/pollistics/PollisticsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.pollistics;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PollisticsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}

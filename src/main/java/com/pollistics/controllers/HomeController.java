91178a97c73dc83e99e2594e2890fe1138fa1e9a
Elias Meire <me@eliasmei.re>
2016-11-12 22:31:37 +0100

    No more database calls in controller

--- src/main/java/com/pollistics/controllers/HomeController.java
+++ src/main/java/com/pollistics/controllers/HomeController.java
@@ -1,26 +1,21 @@
 package com.pollistics.controllers;
 
-import com.pollistics.models.Poll;
-import com.pollistics.repositories.PollRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.List;
+import com.pollistics.services.PollService;
 
 @Controller
 public class HomeController {
-    @Autowired
-    private PollRepository pollRepo;
-
+	@Autowired 
+	PollService pollService;
+	
     @GetMapping(value = "/")
     public String greeting(Model model) {
         model.addAttribute("greeting", "Hello World!");
-        List<Poll> pollList = pollRepo.findAll();
-        model.addAttribute("polls_List",pollList);
+        model.addAttribute("polls", pollService.getAllPolls());
         return "index";
     }
 }
--- src/main/java/com/pollistics/controllers/PollController.java
+++ src/main/java/com/pollistics/controllers/PollController.java
@@ -1,26 +1,21 @@
 package com.pollistics.controllers;
 
-import com.pollistics.models.Poll;
-import com.pollistics.repositories.PollRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.List;
+import com.pollistics.services.PollService;
 
 @Controller
 public class PollController {
-
-    @Autowired
-    private PollRepository pollRepo;
-
+	@Autowired
+	PollService pollService;
+	
     @GetMapping(value = "/polls/{pollId}")
     public String greeting(@PathVariable String pollId, Model model) {
-        Poll polly = pollRepo.findOne(pollId);
-        model.addAttribute("poll", polly);
-        return "poll";
+        model.addAttribute("poll", pollService.getPoll(pollId));
+        return "polls/detail";
     }
 }

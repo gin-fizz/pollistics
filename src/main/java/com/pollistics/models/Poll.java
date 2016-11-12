a99c1efc479b1f17599f98f65a4281e08705501b
Elias Meire <me@eliasmei.re>
2016-11-12 22:28:28 +0100

    Generate getters/setters + make private

--- src/main/java/com/pollistics/models/Poll.java
+++ src/main/java/com/pollistics/models/Poll.java
@@ -9,11 +9,11 @@ import org.springframework.data.mongodb.core.mapping.Document;
 @Document(collection="polls")
 public class Poll {
 	@Id
-    public ObjectId id;
+    private ObjectId id;
 
-    public String name;
-    public HashMap<String,Integer> options;
-    public User user;
+	private String name;
+    private HashMap<String,Integer> options;
+    private User user;
 
     public Poll(String name, HashMap<String,Integer> options) {
         this.name = name;
@@ -28,4 +28,36 @@ public class Poll {
 
     public Poll() {
     }
+    
+    public ObjectId getId() {
+		return id;
+	}
+
+	public void setId(ObjectId id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public HashMap<String, Integer> getOptions() {
+		return options;
+	}
+
+	public void setOptions(HashMap<String, Integer> options) {
+		this.options = options;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
 }

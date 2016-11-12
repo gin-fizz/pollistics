5d6cf07f8f05dbfd578ec2bf453230db3e803277
Elias Meire <me@eliasmei.re>
2016-11-12 22:33:08 +0100

    correct whitespace and imports

--- src/main/java/com/pollistics/PollisticsApplication.java
+++ src/main/java/com/pollistics/PollisticsApplication.java
@@ -2,15 +2,10 @@ package com.pollistics;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 
 @SpringBootApplication
 public class PollisticsApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(PollisticsApplication.class, args);
 	}
-
-
 }

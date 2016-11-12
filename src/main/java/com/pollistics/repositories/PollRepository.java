29fe76431a6ff7ab620e5c209ecd4786af40994f
Elias Meire <me@eliasmei.re>
2016-11-12 22:33:52 +0100

    Annotation + more useful comment

--- src/main/java/com/pollistics/repositories/PollRepository.java
+++ src/main/java/com/pollistics/repositories/PollRepository.java
@@ -4,11 +4,11 @@ package com.pollistics.repositories;
  * Created by Maliek & Elias on 12/11/2016.
  */
 
-import java.util.List;
-
 import com.pollistics.models.Poll;
+import org.springframework.stereotype.Repository;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+@Repository
 public interface PollRepository extends MongoRepository<Poll,String> {
-    //Poll findById(String id);
+    // Insert custom querying methods here
 }

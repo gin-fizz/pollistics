3b209feb4dd337839137b3bcb22795bb9e9ae0a8
Haroen Viaene <hello@haroen.me>
2016-11-19 12:14:47 +0100

    update test
    also:
    fixes #18

--- src/test/java/com/pollistics/controllers/HomeControllerTests.java
+++ src/test/java/com/pollistics/controllers/HomeControllerTests.java
@@ -2,10 +2,8 @@ package com.pollistics.controllers;
 
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.hasItem;

7296d32811c8f32aad0cf828bc7e4bb7115250bd
Elias Meire <me@eliasmei.re>
2016-11-26 19:11:57 +0100

    Try to make logout work (not working). Post to /logout should work according to spring security docs.

--- src/main/resources/js/main.js
+++ src/main/resources/js/main.js
@@ -1,7 +1,30 @@
 /* global options, d3pie */
+var logout = document.getElementById('logout');
+console.log(logout);
+logout.onclick = (e) => {
+	httpPost('/logout')
+		.then((response) => {
+			console.log(response);
+		})		
+		.catch((err) => console.log(err));
+}
 
-var pie = new d3pie('results', { // eslint-disable-line new-cap
-	data: {
-		content: options
-	}
-});
+function httpPost(url) {
+	return new Promise(
+	    function (resolve, reject) {
+	        var request = new XMLHttpRequest();
+	        request.onreadystatechange = function () {
+	            if (this.status === 200) {
+	            	resolve(this.response);
+				} else {
+			        reject(new Error(this.statusText));
+			    }
+			};
+	        request.onerror = function () {
+	        	reject(new Error('XMLHttpRequest Error: '+ this.statusText));
+	        };
+	        request.open('POST', url);
+	        request.send();    
+	});
+}
+

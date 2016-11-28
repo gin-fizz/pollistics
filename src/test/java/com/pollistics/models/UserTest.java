25762ed6f5b4e9d18f212fe1e86969219f6f43a7
Elias Meire <me@eliasmei.re>
2016-11-28 15:58:40 +0100

    User tests

--- src/test/java/com/pollistics/models/UserTest.java
+++ src/test/java/com/pollistics/models/UserTest.java
@@ -1,48 +1,38 @@
 package com.pollistics.models;
 
-//import nl.jqno.equalsverifier.EqualsVerifier;
+import static org.junit.Assert.assertFalse;
+
 import org.junit.Test;
 
-//import java.util.HashMap;
-
 import static org.assertj.core.api.Assertions.assertThat;
-//import static org.junit.Assert.fail;
 
 public class UserTest {
 
 	@Test
 	public void membersTest() {
 		User u = new User("testuser", "testpass");
-		assertThat(u.getName().equals("testuser"));
+		assertThat(u.getUsername().equals("testuser"));
 		assertThat(u.getPassword().equals("testpass"));
-		// todo: this should be hashieshd
 		u.setName("memer");
 		assertThat(u.getName().equals("memer"));
 		u.setPassword("dumbpass");
 		assertThat(u.getPassword().equals("dumbpass"));
-		// todo: this should be hashieshd
+		u.setUsername("testusername");
+		assertThat(u.getUsername().equals("testusername"));
+		u.setEmail("e@a.com");
+		assertThat(u.getEmail().equals("e@a.com"));
+		
 		Poll p = new Poll();
 		u.addPoll(p);
 		assertThat(u.getPolls().contains(p));
 	}
 
-	/*
 	@Test
-	public void equalsContract() {
-		try {
-			HashMap<String, Integer> options = new HashMap<>();
-			options.put("Blauw", 1);
-			Poll p = new Poll("Kleuren", options);
-			HashMap<String, Integer> optionsTwo = new HashMap<>();
-			options.put("Obama", 1);
-			Poll q = new Poll("Verkiezingen", options);
-			EqualsVerifier.forClass(User.class)
-				.withPrefabValues(Poll.class, p, q)
-				.usingGetClass()
-				.verify();
-		} catch (Exception e) {
-			fail(e.getMessage());
-		}
+	public void testEquals() {
+		User u1 = new User("username","e@a.com","password");
+		User u2 = new User("username","e@a.com","password");
+		assertFalse(u1.equals(new Poll()));
+		assertFalse(u1.equals(u2));
+		assertFalse(u1.hashCode() == u2.hashCode());
 	}
-	*/
 }

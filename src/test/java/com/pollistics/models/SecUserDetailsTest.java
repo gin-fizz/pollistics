a81d432e2ce26f65badb61c69d3463a0ec4125c5
Haroen Viaene <hello@haroen.me>
2016-11-28 16:11:17 +0100

    simplify tests, true will mean that test passes

--- src/test/java/com/pollistics/models/SecUserDetailsTest.java
+++ src/test/java/com/pollistics/models/SecUserDetailsTest.java
@@ -1,12 +1,12 @@
 package com.pollistics.models;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class SecUserDetailsTest {
 	@Test
-	public void membersTest() {			
+	public void membersTest() {
 		User u = new User("testuser", "testpass");
 		User u2 = new User("testuser2", "testpass2");
 		SecUserDetails uSec = new SecUserDetails(u);
@@ -16,9 +16,9 @@ public class SecUserDetailsTest {
 		assertThat(uSec.getPassword().equals(u2.getPassword()));
 		assertThat(uSec.getUsername().equals(u2.getUsername()));
 		assertThat(uSec.getAuthorities() == null);
-		assertThat(uSec.isAccountNonExpired() == true);
-		assertThat(uSec.isAccountNonLocked() == true);
-		assertThat(uSec.isCredentialsNonExpired() == true);
-		assertThat(uSec.isEnabled() == true);
+		assertThat(uSec.isAccountNonExpired());
+		assertThat(uSec.isAccountNonLocked());
+		assertThat(uSec.isCredentialsNonExpired());
+		assertThat(uSec.isEnabled());
 	}
 }

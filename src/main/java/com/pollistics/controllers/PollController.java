8c84ebc00f54d0bb6a6cb95e8c21cc0e7017688b
maliek <maliek.meersschaert@hotmail.com>
2016-11-28 21:19:50 +0100

    Restiction of duplicate votes

--- src/main/java/com/pollistics/controllers/PollController.java
+++ src/main/java/com/pollistics/controllers/PollController.java
@@ -5,19 +5,27 @@ import com.pollistics.services.PollService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
+import java.util.stream.Stream;
 
 @Controller
 public class PollController {
 	@Autowired
 	private PollService pollService;
+	private Cookie myCookie = new Cookie("id", "");
 
 	@GetMapping(value = {"/polls/{pollId}", "/{pollId}"})
 	public String pollDetail(@PathVariable String pollId, Model model, HttpServletResponse response) {
@@ -45,10 +53,29 @@ public class PollController {
 	}
 
 	@PostMapping(value = "/polls/vote/{pollId}")
-	public String voteOptions(@PathVariable String pollId, HttpServletRequest request, Model model) {
-		String option = request.getParameter("option");
-		Poll p = pollService.getPoll(pollId);
-		pollService.voteOption(p, option);
-		return "redirect:/" + pollId;
+	public String voteOptions(@CookieValue(value = "id", defaultValue = "") String cookieIdValue,@PathVariable String pollId, HttpServletRequest request, HttpServletResponse response, Model model) {
+		if (myCookie.getValue().contains(pollId)) {
+			Poll p = pollService.getPoll(pollId);
+			model.addAttribute("msg","You already voted for poll: " + p.getName());
+			model.addAttribute("pollId", pollId);
+			response.setStatus(400);
+			return "error/400";
+		} else {
+			if (myCookie.getValue() == "") {
+				cookieIdValue = pollId;
+			} else {
+				cookieIdValue = pollId + "-" + cookieIdValue;
+			}
+			final int expiryTimeCookie = 60 * 60 * 24;
+			final String cookiePath = "/";
+			myCookie.setValue(cookieIdValue);
+			myCookie.setMaxAge(expiryTimeCookie);
+			myCookie.setPath(cookiePath);
+			response.addCookie(myCookie);
+			String option = request.getParameter("option");
+			Poll p = pollService.getPoll(pollId);
+			pollService.voteOption(p, option);
+			return "redirect:/" + pollId;
+		}
 	}
 }

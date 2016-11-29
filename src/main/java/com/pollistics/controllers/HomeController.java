dda07bfa4f2cd282cfb4c6b2afd2d7c0d513e2d0
Haroen Viaene <hello@haroen.me>
2016-11-29 13:56:23 +0100

    delete unused code to list all polls at /

--- src/main/java/com/pollistics/controllers/HomeController.java
+++ src/main/java/com/pollistics/controllers/HomeController.java
@@ -1,17 +1,11 @@
 package com.pollistics.controllers;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import com.pollistics.services.PollService;
-
 @Controller
 public class HomeController {
-	@Autowired
-	private PollService pollService;
-
 	@GetMapping(value = "/")
 	public String greeting(Model model) {
 		return "index";

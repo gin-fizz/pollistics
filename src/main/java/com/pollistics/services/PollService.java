077fa6b060cbc24701c7616585cae3a43357f029
Elias Meire <eliasmeire.dbz@gmail.com>
2016-12-05 16:23:20 +0100

    importje

--- src/main/java/com/pollistics/services/PollService.java
+++ src/main/java/com/pollistics/services/PollService.java
@@ -6,7 +6,6 @@ import com.pollistics.repositories.PollRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 

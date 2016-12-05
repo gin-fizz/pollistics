f7352099ebb1ba4edc348c65073722e6cff82a8b
Elias Meire <eliasmeire.dbz@gmail.com>
2016-12-05 16:12:17 +0100

    add findByUser method

--- src/main/java/com/pollistics/repositories/PollRepository.java
+++ src/main/java/com/pollistics/repositories/PollRepository.java
@@ -5,10 +5,15 @@ package com.pollistics.repositories;
  */
 
 import com.pollistics.models.Poll;
+import com.pollistics.models.User;
+
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 @Repository
 public interface PollRepository extends MongoRepository<Poll,String> {
-	// Insert custom querying methods here
+	List<Poll> findByUser(User user);
 }

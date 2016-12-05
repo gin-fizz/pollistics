98764eb23ab620f68bef41fb1f78b17eaa474521
Haroen Viaene <hello@haroen.me>
2016-12-05 22:59:40 +0100

    this *could* make it unique

--- src/main/java/com/pollistics/models/Poll.java
+++ src/main/java/com/pollistics/models/Poll.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import org.bson.types.ObjectId;
 import org.hibernate.validator.constraints.Length;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.HashMap;
@@ -18,6 +19,8 @@ public class Poll {
 	private String title;
 	private HashMap<String,Integer> options;
 	private User user;
+
+	@Indexed(unique = true)
 	private String slug;
 
 	// todo: orden these constructors with `this(...arg)`

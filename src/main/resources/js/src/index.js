5656fce11c540d995ef1462c6a3a1c4ed2915736
Haroen Viaene <hello@haroen.me>
2016-12-07 11:54:50 +0100

    start transition to rollem

---
+++ src/main/resources/js/src/index.js
@@ -0,0 +1,12 @@
+import {setSlug} from './lib/set-slug';
+import {getCombo} from './lib/meme-words';
+import {addNewPollInput} from "./lib/poll-inputs";
+
+
+addNewPollInput();
+
+document.getElementById('slug').value = getCombo();
+
+const titleInput = document.querySelector('.poll--title input');
+
+titleInput.addEventListener('input', setSlug);
---
+++ src/main/resources/rollem.config.js
@@ -0,0 +1,53 @@
+import * as glob from 'glob';
+
+import babel from 'rollup-plugin-babel';
+import nodeResolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+import uglify from 'rollup-plugin-uglify';
+
+const plugins = [
+	babel(),
+
+	nodeResolve({
+		jsnext: true,
+		main: true
+	}),
+
+	commonjs({
+		// non-CommonJS modules will be ignored, but you can also
+		// specifically include/exclude files
+		include: 'node_modules/**',  // Default: undefined
+		exclude: [ 'node_modules/foo/**', 'node_modules/bar/**' ],  // Default: undefined
+
+		// search for files other than .js files (must already
+		// be transpiled by a previous plugin!)
+		extensions: [ '.js', '.coffee' ],  // Default: [ '.js' ]
+
+		// if true then uses of `global` won't be dealt with by this plugin
+		ignoreGlobal: false,  // Default: false
+
+		// if false then skip sourceMap generation for CommonJS modules
+		sourceMap: false,  // Default: true
+
+		// explicitly specify unresolvable named exports
+		// (see below for more details)
+		namedExports: { './module.js': ['foo', 'bar' ] }  // Default: undefined
+	}),
+
+	uglify()
+];
+
+const sourceFolder = 'js/src';
+const distFolder = 'js/dist';
+const srcFiles = glob.sync(`${sourceFolder}/*.js`);
+
+export default srcFiles.map(file => {
+	var distRegex = new RegExp(`^${sourceFolder}`);
+	return {
+		entry: file,
+		dest: file.replace(distRegex, distFolder),
+		format: 'iife',
+		moduleName: 'pollistics',
+		plugins
+	};
+});

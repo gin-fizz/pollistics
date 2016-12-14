cb8d48b564cce9e495e38294953962acb06862bb
Haroen Viaene <hello@haroen.me>
2016-12-14 10:31:56 +0100

    make graph come up in the first place :smile:

--- src/main/resources/js/src/lib/visualisation.js
+++ src/main/resources/js/src/lib/visualisation.js
@@ -1,57 +1,56 @@
-import d3 from 'd3';
+import * as d3 from "d3";
 
-const pieDiv = d3.select('body').append('div').attr('class', 'toolTip');
+export function visualise(data) {
+	const pieDiv = d3.select('body').append('div').attr('class', 'toolTip');
 
-const pieWidth = 500;
-const pieHeight = 500;
-const pieRadius = Math.min(pieWidth, pieHeight) / 2;
+	const pieWidth = 500;
+	const pieHeight = 500;
+	const pieRadius = Math.min(pieWidth, pieHeight) / 2;
 
-function getRandomColor() {
-	const letters = '0123456789ABCDEF';
-	let color = '#';
-	for (let i = 0; i < 6; i++ ) {
-		color += letters[Math.floor(Math.random() * 16)];
+	function getRandomColor() {
+		const letters = '0123456789ABCDEF';
+		let color = '#';
+		for (let i = 0; i < 6; i++) {
+			color += letters[Math.floor(Math.random() * 16)];
+		}
+		return color;
 	}
-	return color;
-}
 
-const arc = d3.arc()
+	const arc = d3.arc()
 	.outerRadius(pieRadius - 10)
 	.innerRadius(pieRadius - 70);
 
-const pie = d3.pie()
-	.sort(null)
+	const pie = d3.pie()
+	.sort(null)te
 	.value(d => d.value);
 
-const svgPie = d3.select('body').append('svg')
+	const svgPie = d3.select('body').append('svg')
 	.attr('width', pieWidth)
 	.attr('height', pieHeight)
 	.append('g')
 	.attr('transform', `translate(${pieWidth / 2},${pieHeight / 2})`);
 
-const g = svgPie.selectAll('.arc')
+	const g = svgPie.selectAll('.arc')
 	.data(pie(data))
 	.enter().append('g')
 	.attr('class', 'arc');
 
-g.append('path')
+	g.append('path')
 	.attr('d', arc)
 	.style('fill', d => getRandomColor(d.data.label));
 
-g
-	.on('mousemove', d => {
+	g.on('mousemove', d => {
 		pieDiv.style('left', `${d3.event.pageX+10}px`);
 		pieDiv.style('top', `${d3.event.pageY-25}px`);
 		pieDiv.style('display', 'inline-block');
-		pieDiv.html(`${d.data.label}<br>${d.value}Vote(s)`);
+		pieDiv.html(`${d.data.label}<br>${d.value} vote${d.data.value > 1 ? 's' : ''}`);
 	});
 
-g
-	.on('mouseout', d => {
+	g.on('mouseout', d => {
 		pieDiv.style('display', 'none');
 	});
-g.append('text')
-	.attr('transform', (d) => {
+
+	g.append('text').attr('transform', (d) => {
 		console.log(d);
 		return `translate(${arc.centroid(d)})`;
 	})
@@ -59,88 +58,87 @@ g.append('text')
 	.text((d) => d.data.label)
 	.attr('class', 'value');
 
-const div = d3.select('body').append('div').attr('class', 'toolTip');
+	const div = d3.select('body').append('div').attr('class', 'toolTip');
 
-const axisMargin = 20;
-const margin = 40;
-const valueMargin = 4;
-let width = parseInt(d3.select('body').style('width'), 10);
-let height = parseInt(d3.select('body').style('height'), 10);
-const barHeight = (height-axisMargin-margin*2)* 0.4/data.length;
-const barPadding = (height-axisMargin-margin*2)*0.4/data.length;
-var data;
-let bar;
-let svg;
-let scale;
-let xAxis;
-let labelWidth = 0;
-width *= .5;
-height *= .8;
-console.log(width);
-max = d3.max(data, d => d.value);
+	const axisMargin = 20;
+	const margin = 40;
+	const valueMargin = 4;
+	let width = parseInt(d3.select('body').style('width'), 10);
+	let height = parseInt(d3.select('body').style('height'), 10);
+	const barHeight = (height - axisMargin - margin * 2) * 0.4 / data.length;
+	const barPadding = (height - axisMargin - margin * 2) * 0.4 / data.length;
+	let bar;
+	let svg;
+	let scale;
+	let xAxis;
+	let labelWidth = 0;
+	width *= .5;
+	height *= .8;
+	console.log(width);
+	const max = d3.max(data, d => d.value);
 
-svg = d3.select('body')
+	svg = d3.select('body')
 	.append('svg')
 	.attr('width', width)
 	.attr('height', height);
 
 
-bar = svg.selectAll('g')
+	bar = svg.selectAll('g')
 	.data(data)
 	.enter()
 	.append('g');
 
-bar.attr('class', 'bar')
-	.attr('cx',0)
+	bar.attr('class', 'bar')
+	.attr('cx', 0)
 	.attr('transform', (d, i) => `translate(${margin},${i * (barHeight + barPadding) + barPadding})`);
 
-bar.append('text')
+	bar.append('text')
 	.attr('class', 'value2')
 	.attr('y', barHeight / 2)
 	.attr('dy', '.35em') //vertical align middle
 	.text(d => d.label).each(function() {
-	labelWidth = Math.ceil(Math.max(labelWidth, this.getBBox().width));
-});
+		labelWidth = Math.ceil(Math.max(labelWidth, this.getBBox().width));
+	});
 
-scale = d3.scaleLinear()
-	.domain([0, max + max *0.20])
-	.range([0, width - margin*2 - labelWidth]);
+	scale = d3.scaleLinear()
+	.domain([0, max * 1.2])
+	.range([0, width - margin * 2 - labelWidth]);
 
-xAxis = d3.axisBottom()
+	xAxis = d3.axisBottom()
 	.scale(scale)
-	.tickSize(-height + 2*margin + axisMargin)
+	.tickSize(-height + 2 * margin + axisMargin)
 
-bar.append('rect')
+	bar.append('rect')
 	.attr('transform', `translate(${labelWidth}, 0)`)
 	.attr('height', barHeight)
 	.attr('width', d => scale(d.value));
 
-bar.append('text')
+	bar.append('text')
 	.attr('class', 'value')
 	.attr('y', barHeight / 2)
 	.attr('dx', -valueMargin + labelWidth) //margin right
 	.attr('dy', '.35em') //vertical align middle
 	.attr('text-anchor', 'end')
 	.text(d => `${d.value} Vote(s)`)
-	.attr('x', function(d){
+	.attr('x', function(d) {
 		const width = this.getBBox().width;
 		return Math.max(width + valueMargin, scale(d.value));
 	});
 
-bar
+	bar
 	.on('mousemove', d => {
 		div.style('left', `${d3.event.pageX+10}px`);
 		div.style('top', `${d3.event.pageY-25}px`);
 		div.style('display', 'inline-block');
 		div.html(`${d.label}<br>${d.value}Vote(s)`);
 	});
-bar
+	bar
 	.on('mouseout', d => {
 		div.style('display', 'none');
 	});
 
-svg.insert('g',':first-child')
+	svg.insert('g', ':first-child')
 	.attr('class', 'axisHorizontal')
 	.attr('transform', `translate(${margin + labelWidth},${height - axisMargin - margin})`)
 	.call(xAxis);
-
+}
--- src/main/resources/js/src/results.js
+++ src/main/resources/js/src/results.js
@@ -1,3 +1,5 @@
-import {addCopy} from "./lib/social";
+import {addCopy} from './lib/social';
+import {visualise} from './lib/visualisation';
 
 addCopy(decodeURIComponent(document.URL), '#social');
+visualise(window.data);

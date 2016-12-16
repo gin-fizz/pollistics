36af5196bca69f3f8fd18d9237058b501e45562a
Haroen Viaene <fingebimus@me.com>
2016-12-16 10:16:35 +0100

    put back how to use poll

--- src/main/resources/js/src/results.js
+++ src/main/resources/js/src/results.js
@@ -1,3 +1,11 @@
 import {addCopy} from './lib/social';
-//import {poll} from './lib/long-polling.js';
+// import {poll} from './lib/long-polling.js';
+
+// put in this callback a method that will repaint the grap
+// if possible it should only repaint if the data is different
+//
+// poll(`/api/1/polls/${window.id}`, (data) => {
+// 	console.log(data);
+// }, 1000);
+
 addCopy(decodeURIComponent(document.URL), '#social');

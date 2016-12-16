b1dae23c04164f029598fc5ddda268bbfee1211a
Haroen Viaene <fingebimus@me.com>
2016-12-16 18:44:16 +0100

    should be multiple of it

--- src/main/resources/js/src/lib/set-slug.js
+++ src/main/resources/js/src/lib/set-slug.js
@@ -2,6 +2,6 @@ export function setSlug(ev) {
 	let raw = ev.target.value;
 	raw = raw.replace(/\?|\./g, '');
 	raw = raw.replace(/\s|#|\/|:|,/g, '-');
-	raw = raw.replace(/-*/g, '-');
+	raw = raw.replace(/-+/g, '-');
 	document.getElementById('slug').value = raw.toLocaleLowerCase();
 }

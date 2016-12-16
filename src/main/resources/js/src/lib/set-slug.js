8d3a67c0d0e34950c04d90533ae1b23f429fef39
Haroen Viaene <hello@haroen.me>
2016-12-16 15:47:36 +0100

    pattern

--- src/main/resources/js/src/lib/set-slug.js
+++ src/main/resources/js/src/lib/set-slug.js
@@ -1,5 +1,5 @@
 export function setSlug(ev) {
 	let raw = ev.target.value;
-  raw = raw.replace(/\?|\./g, '');
+	raw = raw.replace(/\?|\./g, '');
 	document.getElementById('slug').value = raw.replace(/\s|#|\/|:/g, '-').toLocaleLowerCase();
 }
